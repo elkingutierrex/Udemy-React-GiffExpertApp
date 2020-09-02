9ef74764c24222b64ee510cd38f6bd71621a8472
elkingutierrex <elkin.gutierrex@gmail.com>
2020-09-02 11:17:06 -0500

    202009021116 - git docs agregado (se le cambio el nombre a la carpeta build

---
+++ docs/precache-manifest.6b9da2353072b06f92dbcfe0c14790b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f7bc93abf18bffa2a2fbc8789f0b7f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "542f6a3efe5fdbcdc28a",
+    "url": "/static/css/main.fe241a43.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542f6a3efe5fdbcdc28a",
+    "url": "/static/js/main.1e6387b7.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
